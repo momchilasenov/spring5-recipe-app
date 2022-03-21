70b9b3ead6daa15e935f05625ebb825689e583e9
Momchil Asenov <momchil276@gmail.com>
2022-03-21 21:35:28 +0200

    - add data.sql - fix bug in CategoryRepository

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,7 +1,8 @@
 package guru.springframework.repositories;
 
+import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CategoryRepository extends CrudRepository<CategoryRepository, Long>
+public interface CategoryRepository extends CrudRepository<Category, Long>
 {
 }
