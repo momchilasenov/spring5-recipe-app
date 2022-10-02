14140717419098154df71e1a7f94c2ef0d4e76cd
Momchil Asenov <momchil276@gmail.com>
2022-10-02 19:24:14 +0300

    - add showById API -

--- src/main/java/guru/springframework/service/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/service/RecipeServiceImpl.java
@@ -26,4 +26,11 @@ public class RecipeServiceImpl implements RecipeService
     recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
     return recipeSet;
   }
+
+  @Override
+  public Recipe findById(Long id)
+  {
+    return recipeRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Recipe not found"));
+  }
 }
