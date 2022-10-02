9f85e9b2dc4f35db5c5b37f2075375fe52285b4d
Momchil Asenov <momchil276@gmail.com>
2022-10-02 20:15:41 +0300

    -test

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController
 {
-  private final RecipeService recipeService;
+  private final RecipeService recipeService ;
 
   public IndexController(RecipeService recipeService)
   {
